ad1714b348f175e740be4f9dafc5d6e04df46c3d
Lynn <lynn9388@gmail.com>
2017-09-05 19:29:54 +0800

    Add urioj problem solutions for 1011 to 1015

---
+++ uri-online-judge/1013 The Greatest/main.go
@@ -0,0 +1,44 @@
+/*********************************************************************
+                             The Greatest
+    https://www.urionlinejudge.com.br/judge/en/problems/view/1013
+
+Make a program that reads 3 integer values and present the greatest
+one followed by the message "eh o maior". Use the following formula:
+
+Input
+*****
+The input file contains 3 integer values.
+
+Output
+******
+Print the greatest of these three values followed by a space and the
+message “eh o maior”.
+
++--------------+----------------+
+| SAMPLE INPUT | SAMPLE OUTPUT  |
++--------------+----------------+
+| 7 14 106     | 106 eh o maior |
++--------------+----------------+
+| 217 14 6     | 217 eh o maior |
++--------------+----------------+
+*********************************************************************/
+
+package main
+
+import (
+	"fmt"
+)
+
+func max(x, y int) int {
+	if x > y {
+		return x
+	} else {
+		return y
+	}
+}
+
+func main() {
+	var x, y, z int
+	fmt.Scanf("%d %d %d", &x, &y, &z)
+	fmt.Printf("%d eh o maior\n", max(max(x, y), z))
+}
---
+++ uri-online-judge/1014 Consumption/main.go
@@ -0,0 +1,43 @@
+/*********************************************************************
+                             Consumption
+    https://www.urionlinejudge.com.br/judge/en/problems/view/1014
+
+Calculate a car's average consumption being provided the total distance
+traveled (in Km) and the spent fuel total (in liters).
+
+Input
+*****
+The input file contains two values: one integer value X  representing
+the total distance (in Km) and the second one is a floating point
+number Y  representing the spent fuel total, with a digit after the
+decimal point.
+
+Output
+******
+Present a value that represents the average consumption of a car with
+3 digits after the decimal point, followed by the message "km/l".
+
++--------------+---------------+
+| SAMPLE INPUT | SAMPLE OUTPUT |
++--------------+---------------+
+| 500          | 14.286 km/l   |
+| 35.0         |               |
++--------------+---------------+
+| 2254         | 18.119 km/l   |
+| 124.4        |               |
++--------------+---------------+
+| 4554         | 9.802 km/l    |
+| 464.6        |               |
++--------------+---------------+
+*********************************************************************/
+
+package main
+
+import "fmt"
+
+func main() {
+	var km int
+	var l float64
+	fmt.Scanf("%d\n%f", &km, &l)
+	fmt.Printf("%.3f km/l\n", float64(km)/l)
+}
