b40e864636a3ec8cc694bd11e025b85009439e29
Lynn <lynn9388@gmail.com>
2018-12-16 15:32:22 +0800

    Update project settings

--- uri-online-judge/1020 Age in Days/main.go
+++ uri-online-judge/1020 Age in Days/main.go
@@ -8,8 +8,8 @@ print it in years, months and days, followed by its respective message
 
 Note: only to facilitate the calculation, consider the whole year with
 365 days and 30 days every month. In the cases of test there will
-never a situation that allows 12 months and some days, like 360, 363
-or 364. This is just an exercise for the purpose of testing simple
+never be a situation that allows 12 months and some days, like 360,
+363 or 364. This is just an exercise for the purpose of testing simple
 mathematical reasoning.
 
 Input
